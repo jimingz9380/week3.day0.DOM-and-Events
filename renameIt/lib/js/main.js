8dd2c8088cdcb004d2000e63769321603987d019
Jiming Zheng <jimingz9380@gmail.com>
2022-08-01 13:34:56 -0400

    finsihed renamelt

---
+++ renameIt/lib/js/main.js
@@ -0,0 +1,5 @@
+let namechange = document.querySelector("#item_name");
+namechange.innerHTML = ("Frostfang");
+
+let imgchange = document.getElementById("item_img");
+imgchange.src= "assets/Frostfang_item.png";
---
+++ theBrush/lib/js/main.js
@@ -0,0 +1,5 @@
+let img = document.querySelector("#rengar")
+
+img.onclick = function(){
+    img.src = " assets/rengar.png "
+}
